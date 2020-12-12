f4e5cb66230a9e122ff418f4b0bdae6cab5313f8
Nicole Pizzano <nicolepizzano@Nicoles-iMac.local>
2020-12-12 16:25:22 -0500

    completed task

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-//Choose a random color
+
 const button = document.querySelector('button')
 const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const colors = ['orange','green','blue','yellow','pink', 'purple']
 
-body.style.backgroundColor = 'violet'
+body.style.backgroundcolor = 'violet'
 button.addEventListener('click', changeBackground)
 
 function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+    const colorIndex = parseInt(Math.random()*colors.length)
+    body.style.backgroundColor = colors[colorIndex]
 }
